6b6f6046749cb4d1926e073c330496daeecf9d89
Konstantins Franckevics <cucazik@inbox.lv>
2018-10-31 17:31:47 +0100

    Drafted another solution method

--- cowsandbulls_solver.py
+++ cowsandbulls_solver.py
@@ -1,20 +1,43 @@
 digit_possibilities = [0] * 10
 digit_place_possibilities = [[0] * 4 for i in range(10)]
 
-current_guess = [0,1,2,3]
-print(current_guess)
-cows = int(input("Cows: "))
-bulls = int(input("Bulls: "))
+def get_cows_bulls(guess, solution):
+    cows = 0
+    bulls = 0
+    for i in range(len(guess)):
+        if guess[i] == solution[i]:
+            bulls += 1
+        elif guess[i] in solution:
+            cows += 1
+    return cows, bulls
 
-for number in current_guess:
-    digit_possibilities[number] = (cows + bulls) / 4
+def is_valid_solution(guess, cows, bulls, solution):
+    return (cows, bulls) == get_cows_bulls(guess, solution)
 
-for i in range(4):
-    for j in range(4):
-        if i == j:
-            digit_place_possibilities[current_guess[i]][i] = bulls / 4
-        else:
-            digit_place_possibilities[current_guess[i]][j] = cows / 4
+possible_solutions = []
+for i in range(10000):
+    a = i // 1000
+    b= (i // 100) % 10
+    c = (i // 10) % 10
+    d = i % 10
+    if a not in [b,c,d] and b not in [a,c,d] and c not in [a,b,d] and d not in [a,b,c]:
+        possible_solutions.append((a, b, c, d))
 
-print(digit_possibilities)
-print(digit_place_possibilities)
+while len(possible_solutions) > 1:
+    print(possible_solutions[0])
+    cows = int(input("Cows: "))
+    bulls = int(input("Bulls: "))
+    possible_solutions = [x for x in possible_solutions if is_valid_solution(possible_solutions[0], cows, bulls, x)]
+
+# for number in current_guess:
+#     digit_possibilities[number] = (cows + bulls) / 4
+
+# for i in range(4):
+#     for j in range(4):
+#         if i == j:
+#             digit_place_possibilities[current_guess[i]][i] = bulls / 4
+#         else:
+#             digit_place_possibilities[current_guess[i]][j] = cows / 4
+
+# print(digit_possibilities)
+# print(digit_place_possibilities)
