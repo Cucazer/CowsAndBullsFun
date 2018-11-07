775c2c21fc83ac40b41fbab459606ad19b29fe74
Konstantins Franckevics <cucazik@inbox.lv>
2018-11-07 17:25:31 +0100

    Added value check

--- cowsandbulls_solver.py
+++ cowsandbulls_solver.py
@@ -4,7 +4,7 @@ class CowsAndBullsSolver:
         self.possible_solutions = []
         for i in range(10000):
             a = i // 1000
-            b= (i // 100) % 10
+            b = (i // 100) % 10
             c = (i // 10) % 10
             d = i % 10
             if a not in [b,c,d] and b not in [a,c,d] and c not in [a,b,d] and d not in [a,b,c]:
@@ -17,10 +17,12 @@ class CowsAndBullsSolver:
         return "".join(map(str, self.possible_solutions[0]))
 
     def make_guess(self, cows = None, bulls = None):
-        print(f"{self.get_current_guess()}")
         if cows is None and bulls is None:
+            print(f"{self.get_current_guess()}")
             cows = int(input("Cows: "))
             bulls = int(input("Bulls: "))
+        if not self.is_valid_cows_bulls_count(cows, bulls):
+            raise ValueError("Impossible cows and/or bulls count")
         self.possible_solutions = [x for x in self.possible_solutions if self.is_valid_solution(self.possible_solutions[0], cows, bulls, x)]
     
     def get_cows_bulls(self, guess, solution):
@@ -35,6 +37,13 @@ class CowsAndBullsSolver:
 
     def is_valid_solution(self, guess, cows, bulls, solution):
         return (cows, bulls) == self.get_cows_bulls(guess, solution)
+
+    def is_valid_cows_bulls_count(self, cows, bulls):
+        if cows < 0 or bulls < 0 or cows + bulls > 4:
+            return False
+        if cows == 1 and bulls == 3:
+            return False
+        return True
 
     def solve(self):
         while not self.is_solved():
